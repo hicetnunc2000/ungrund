4f3f3d0a6729da96e861dbc6dd7dc0be35be62d0
hicetnunc2000 <180111973@aluno.unb.br>
2020-06-25 06:07:57 -0300

    latest

--- routes/fa2_route.py
+++ routes/fa2_route.py
@@ -6,12 +6,14 @@ from pytezos import pytezos
 from pytezos.operation.result import OperationResult
 from flask import Flask
 from flask_restx import fields, Resource, Api, Namespace
+from controllers.validate import Validate
 
 import requests
 import urllib
 
 pytezos = pytezos
 OperationResult = OperationResult
+v = Validate()
 
 api = Namespace('fa2', description='publish and other entrypoints')
 
@@ -21,7 +23,7 @@ class publish_fa2(Resource):
     def get(self):
         try:
             contract = Contract.from_file('./smart_contracts/fa2.tz')
-            op = pytezos.origination(script=contract.script(storage={ "administrator" : pytezos.key.public_key_hash(), "all_tokens" : 0, "ledger" : {}, "paused" : False, "tokens" : {}, "totalSupply" : 0  })).autofill().sign().inject(_async=False, num_blocks_wait=2)
+            op = pytezos.origination(script=contract.script(storage={ "administrator" : pytezos.key.public_key_hash(), "all_tokens" : 0, "ledger" : {}, "paused" : False, "tokens" : {}, "totalSupply" : 0  })).fill().sign().inject(_async=False, num_blocks_wait=2)
             originated_kt = OperationResult.originated_contracts(op)
             return v.filter_response(op)
         except:
